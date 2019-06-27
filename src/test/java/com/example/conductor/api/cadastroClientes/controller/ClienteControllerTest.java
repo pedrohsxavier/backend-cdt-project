ec54b411688386c2f14b12508e2f4ef452c88bb0
Neil Junior <CONDUCTOR\neil.junior@njp00104.conductor.tecnologia>
2019-06-27 12:00:29 -0300

    Doing tests

--- src/test/java/com/example/conductor/api/cadastroClientes/controller/ClienteControllerTest.java
+++ src/test/java/com/example/conductor/api/cadastroClientes/controller/ClienteControllerTest.java
@@ -12,12 +12,23 @@ public class ClienteControllerTest {
     public ErrorCollector error = new ErrorCollector();
 
     @Test
-    public void addingTest(){
+    public void addingTest() throws Exception{
         //Cenário de sucesso
         Cliente c = new Cliente("Neil", "neil@neil.com", "123456", "(83)99999-8888");
 
         error.checkThat(c.getSenha(), CoreMatchers.is(CoreMatchers.equalTo("123456")));
-        
+        error.checkThat(c.getNome(), CoreMatchers.is(CoreMatchers.notNullValue()));
+
+        /* !!Tratando exceções!! */
+        /*
+        * No ponto possível da exceção:
+        * try{
+        *   Momento da exceção
+        * }catch(Exception e){
+        *   Assert.assertThat(e.getMessage(), Corematchers.is("Mensagem de exceção");
+        * }
+        * Para evitar falso positivo, deve-se incluir o Assert.assertThat("Mensagem") dependendo do que é esperado.
+         */
     }
 
 
