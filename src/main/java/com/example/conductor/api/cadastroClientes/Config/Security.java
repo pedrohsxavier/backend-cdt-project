056ee975a935cec69ea53b4280347877aa0e9303
neilprado <neiljohn17@gmail.com>
2019-07-02 11:26:35 -0300

    Adding oauth2

--- src/main/java/com/example/conductor/api/cadastroClientes/Config/Security.java
+++ src/main/java/com/example/conductor/api/cadastroClientes/Config/Security.java
@@ -1,11 +1,33 @@
 package com.example.conductor.api.cadastroClientes.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
-public class Security {
+public class Security extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin")
+                .password("123").roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
